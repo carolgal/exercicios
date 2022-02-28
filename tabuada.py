e0bc14e4f4f85555c758d9391113a761d0d99694
carolerolero <galeskar@gmail.com>
2022-02-28 16:05:47 -0300

    exerci py

---
+++ tabuada.py
@@ -0,0 +1,5 @@
+tabuada=int(input("Qual tabuada você quer? "))
+print("Tabuada do número: ", tabuada)
+
+for valor in range(1, 11, 1):
+    print(str(tabuada) + "x" + str(valor) + " = " + str(tabuada*valor))
